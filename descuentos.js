3870696fcbd2e41f44d9c1d5f7e4ba95eece1d22
Patrick MCQ <patrickmcqbooks@gmail.com>
2022-05-19 08:54:37 -0600

    Ingresando HTML desde Javascript

--- descuentos.js
+++ descuentos.js
@@ -8,6 +8,18 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultPrice");
+    resultP.innerText = "El precio con descuento es de Q." + precioConDescuento;
+}
 /**console.log({
     precioOriginal,
     descuento,
